96a33699e6d2e7d0acaac6a0af6c2f54b82c1a0f
Cobbie Quintos <cobbiequintos@gmail.com>
2019-12-31 02:46:30 +0800

    KF spacing issue bottom b4 cards

--- src/pages/KeepingFaith.vue
+++ src/pages/KeepingFaith.vue
@@ -147,7 +147,7 @@ This importance of faith is evidenced in the displaced Maranao’s simple <span 
 Despite the misinterpretations of individuals from within and outside of the faith, Islam is a peaceful religion that has sustained the displaced Maranaos throughout two long years of rehabilitation. The problems of Marawi’s recovery process run deep, but regular Filipino citizens can begin to play their part by practicing cultural and religious sensitivity—the foundations of solidarity, peace, and empathy. <br>
     </div>
   </section>
-    <CardsSection />
+    <CardsSection style="margin-top:20px;" />
   </PageLayout>
 </template>
 
